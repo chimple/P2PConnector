2844db4d7543e12d289ded301ff4b7ee5d40fb31
suhas4090 <suhas@sutara.org>
2018-04-30 10:32:56 +0530

    changes

---
+++ app/src/main/java/p2p/chimple/org/p2pconnector/P2PActivity/ChatActivity.java
@@ -0,0 +1,15 @@
+package p2p.chimple.org.p2pconnector.P2PActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import p2p.chimple.org.p2pconnector.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
