f1a5eb58642937b44d89f96d547ccaa9f7bd52b9
Shyamal Upadhyaya <Shyamal.Upadhyaya@openlane.com>
2018-04-30 08:19:43 +0530

    debug-sync

--- app/src/main/java/p2p/chimple/org/p2pconnector/db/DatabaseInitializer.java
+++ app/src/main/java/p2p/chimple/org/p2pconnector/db/DatabaseInitializer.java
@@ -31,7 +31,7 @@ public class DatabaseInitializer {
         SharedPreferences pref = context.getSharedPreferences(P2P_SHARED_PREF, 0);
         String generateUserId = pref.getString("USER_ID", null); // getting String
         String fileName = P2PSyncManager.generateUserPhotoFileName(generateUserId);
-        P2PSyncManager.createProfilePhoto(fileName, "HEllo - welcome".getBytes(), context);
+        P2PSyncManager.createProfilePhoto(fileName, "hello - welcome".getBytes(), context);
         AssetManager assetManager = context.getAssets();
         InputStream inputStream = null;
         try {
