d7080237b4b720c8472c56b9808d45a9921320f1
suhas4090 <suhas@sutara.org>
2018-05-10 11:48:21 +0530

    new update

--- app/src/main/java/p2p/chimple/org/p2pconnector/P2PActivity/LoginActivity.java
+++ app/src/main/java/p2p/chimple/org/p2pconnector/P2PActivity/LoginActivity.java
@@ -1,40 +1,224 @@
 package p2p.chimple.org.p2pconnector.P2PActivity;
 
+import android.app.Activity;
+import android.arch.persistence.room.Room;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import p2p.chimple.org.p2pconnector.MainActivity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import p2p.chimple.org.p2pconnector.P2PActivity.ActionTypeActivity;
+import p2p.chimple.org.p2pconnector.P2PActivity.NeighbourList;
+import p2p.chimple.org.p2pconnector.P2PActivity.TakeProfilePic;
 import p2p.chimple.org.p2pconnector.R;
+import p2p.chimple.org.p2pconnector.db.AppDatabase;
+import p2p.chimple.org.p2pconnector.db.P2PDBApi;
+import p2p.chimple.org.p2pconnector.db.P2PDBApiImpl;
+import p2p.chimple.org.p2pconnector.db.dao.P2PSyncInfoDao;
+import p2p.chimple.org.p2pconnector.db.entity.P2PSyncInfo;
+import p2p.chimple.org.p2pconnector.db.entity.P2PUserIdMessage;
+import p2p.chimple.org.p2pconnector.scheduler.JobUtils;
+import p2p.chimple.org.p2pconnector.sync.P2PSyncManager;
+import p2p.chimple.org.p2pconnector.sync.SyncUtils;
 
-public class LoginActivity extends AppCompatActivity {
+import static junit.framework.Assert.assertEquals;
+import static p2p.chimple.org.p2pconnector.application.P2PApplication.IMMEDIATE_JOB_TIMINGS;
+import static p2p.chimple.org.p2pconnector.application.P2PApplication.REGULAR_JOB_TIMINGS;
+import static p2p.chimple.org.p2pconnector.sync.P2PSyncManager.P2P_SHARED_PREF;
+import static p2p.chimple.org.p2pconnector.sync.P2PSyncManager.customStatusUpdateEvent;
+import static p2p.chimple.org.p2pconnector.sync.P2PSyncManager.customTimerStatusUpdateEvent;
+import static p2p.chimple.org.p2pconnector.application.P2PApplication.db;
 
-    Button regUser,newUser;
+public class LoginActivity extends Activity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private LoginActivity that = this;
+
+    private Context context;
+    ImageView imageView;
+    ListView listView;
+    String[] listItem=null;
+    ArrayAdapter<String> adapter=null;
+
+    private P2PDBApiImpl p2pdbapi = null;
+    private P2PSyncInfoDao p2PSyncInfoDao;
+    private P2PSyncInfo p2PSyncInfo;
+    static final int CAM_REQUEST = 1;
+
+    String fileName=null;
+    String userId=null;
+    String deviceId=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter(customStatusUpdateEvent));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageTimerReceiver, new IntentFilter(customTimerStatusUpdateEvent));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        regUser = (Button) findViewById(R.id.regUser);
-        newUser = (Button) findViewById(R.id.newUser);
+        imageView = (ImageView) findViewById(R.id.image_view);
 
-        regUser.setOnClickListener(new View.OnClickListener() {
+        listView = (ListView) findViewById(R.id.list);
+
+
+        p2PSyncInfoDao = db.p2pSyncDao();
+        p2pdbapi = new P2PDBApiImpl(db,getApplicationContext());
+
+        SharedPreferences pref = getSharedPreferences(P2P_SHARED_PREF, 0);
+        fileName = pref.getString("PROFILE_PHOTO", null); // getting String
+        userId = pref.getString("USER_ID", null); // getting String
+        deviceId = pref.getString("DEVICE_ID", null); // getting String
+        Log.i("buttonAllUsers:","PROFILE_PHOTO filename :"+fileName+", USER_ID :  "+userId+", DEVICE_ID :  "+deviceId);
+
+
+        Button showIPButton = (Button) findViewById(R.id.button3);
+        showIPButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                SyncUtils.printLocalIpAddresses(that);
+            }
+        });
+
+        Button clearButton = (Button) findViewById(R.id.button2);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ((TextView) findViewById(R.id.debugdataBox)).setText("");
+            }
+        });
+
+
+        Button toggleButton = (Button) findViewById(R.id.buttonToggle);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JobUtils.scheduledJob(getApplicationContext(), IMMEDIATE_JOB_TIMINGS);
+            }
+        });
+
+//        Button buttonAdd = (Button) findViewById(R.id.buttonAdd);
+//
+//        buttonAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Intent camera = new Intent(getApplicationContext(), TakeProfilePic.class);
+//                startActivity(camera);
+//            }
+//        });
+
+//        Button buttonAllUsers = (Button) findViewById(R.id.buttonAllUsers);
+//
+//        buttonAllUsers.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+
+        List<String> users = p2pdbapi.getUsers();
+        Toast.makeText(getApplicationContext(),String.valueOf(users.size()),Toast.LENGTH_LONG).show();
+        for (int i = 0; i < users.size(); i++) {
+            Log.i("MainActivity AllUsers", users.get(i));
+            listItem = users.toArray(new String[i]);
+        }
+        adapter = new ArrayAdapter<String>(getBaseContext(),
+                android.R.layout.simple_list_item_1, listItem);
+        listView.setAdapter(adapter);
+//            }
+//        });
+
+//        Button buttonNeighbour = (Button) findViewById(R.id.buttonNeighbour);
+//
+//        buttonNeighbour.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Object obj = new Object();
+//                obj = P2PSyncManager.getInstance(getApplicationContext()).getNeighbours();
+//                Log.i("buttonNeighbour", String.valueOf(obj));
+//            }
+//        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // TODO Auto-generated method stub
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),"MainActivity: "+value, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), NeighbourList.class);
+                intent.putExtra("MyId",value);
                 startActivity(intent);
             }
         });
 
-        newUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TakeProfilePic.class);
-                startActivity(intent);
-            }
-        });
+        this.execute();
     }
+
+    public void execute() {
+        JobUtils.scheduledJob(getApplicationContext(), REGULAR_JOB_TIMINGS);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        updateStatus(TAG, "Destroying MainActivity");
+    }
+
+    public void updateStatus(String who, String line) {
+        final String logWho = who;
+        final String status = line;
+        runOnUiThread(new Thread(new Runnable() {
+            public void run() {
+//                ((TextView) findViewById(R.id.debugdataBox)).append(logWho + " : " + status + "\n");
+            }
+        }));
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            String who = intent.getStringExtra("who");
+            String line = intent.getStringExtra("line");
+            that.updateStatus(who, line);
+        }
+    };
+
+    public void updateTimerStatus(final int timeCounter) {
+        runOnUiThread(new Thread(new Runnable() {
+            public void run() {
+                ((TextView) findViewById(R.id.TimeBox)).setText("T: " + timeCounter);
+            }
+        }));
+    }
+
+    private BroadcastReceiver mMessageTimerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int timeCounter = intent.getIntExtra("timeCounter", -1);
+            that.updateTimerStatus(timeCounter);
+        }
+    };
 }
+
